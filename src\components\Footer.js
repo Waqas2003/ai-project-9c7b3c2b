f14cdb8ba0405863d6109cff699d900da075730c
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:03:21 +0500

    Add src\components\Footer.js

---
+++ src\components\Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="bg-green-500 p-4 text-white text-center">
+      &copy; 2023 Pakistan Cricket Team
+    </footer>
+  );
+}
+
+export default Footer;
