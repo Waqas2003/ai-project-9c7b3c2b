5b19c97b9a517ca638b18d536f4c1a7059e22041
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:03:28 +0500

    Add src\data\schedule.js

---
+++ src\data\schedule.js
@@ -0,0 +1,15 @@
+export default [
+  {
+    id: 1,
+    date: '2023-02-10',
+    opponent: 'India',
+    venue: 'National Stadium, Karachi'
+  },
+  {
+    id: 2,
+    date: '2023-02-15',
+    opponent: 'Australia',
+    venue: 'Gaddafi Stadium, Lahore'
+  },
+  // ...
+];
