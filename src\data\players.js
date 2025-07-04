d779e667166946b352e8741fce69557557641326
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-07-05 01:03:26 +0500

    Add src\data\players.js

---
+++ src\data\players.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    id: 1,
+    name: 'Babar Azam',
+    role: 'Captain, Batsman'
+  },
+  {
+    id: 2,
+    name: 'Mohammad Rizwan',
+    role: 'Wicket-keeper, Batsman'
+  },
+  {
+    id: 3,
+    name: 'Shaheen Afridi',
+    role: 'Bowler'
+  },
+  // ...
+];
